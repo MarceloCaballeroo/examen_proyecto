9333a660881dc73437b4fdf355f52cbedf958dba
MarceloCaballeroo <marc-caballero@duocuc.cl>
2024-07-17 22:34:57 -0400

    arreglado carrito

--- examen/vehiculos/urls.py
+++ examen/vehiculos/urls.py
@@ -13,11 +13,6 @@ urlpatterns = [
     path('cart/', views.cart_detail, name='cart_detail'),
     path('cart/add/<int:vehiculo_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-
-    path('cart/', cart_detail, name='cart_detail'),
-    path('cart/add/<int:vehiculo_id>/', add_to_cart, name='add_to_cart'),
-    path('cart/remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
-    path('cart/update/<int:item_id>/', views.update_cart_item, name='update_cart_item'),
     
     path('checkout/', views.checkout, name='checkout'),
 ]
